c2930844f5c2c17be19812924a45ab66f47d57a0
cleidianaa <cleidianapassos@outlook.pt>
2022-01-13 15:16:40 +0000

    inicio de projeto

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+/*-----------About Tabs -------------*/
+
+const tabsContainer = document.querySelector(".about-tabs"),
+aboutSection = document.querySelector(".about-section")
+
+tabsContainer.addEventListener("click",(e) =>{
+ console.log(e.target)
+})
